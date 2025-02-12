a2fc06f7df2f35517f38ba93cfaf65fec59bfed2
Wene Alves <weneplay5@gmail.com>
2025-02-12 09:33:56 -0300

    adicionado assets de inimigos do game

--- tests/player/main.go
+++ tests/player/main.go
@@ -29,25 +29,42 @@ const (
 )
 
 var (
-	playerImage *ebiten.Image
-	tilesImage  *ebiten.Image
+	playerImage            *ebiten.Image
+	tilesImage             *ebiten.Image
+	mushroomEnemyImageIdle *ebiten.Image
+	mushroomEnemyImageRun  *ebiten.Image
 )
 
 func init() {
-	playerImage = loadImage(images.Runner_png)
-	tilesImage = loadImage(images.Tiles_png)
+	playerImage = loadImageBytes(images.Runner_png)
+	tilesImage = loadImageBytes(images.Tiles_png)
+	mushroomEnemyImageIdle = loadImage("assets/Enemies/Mushroom/Idle (32x32).png")
+	mushroomEnemyImageRun = loadImage("assets/Enemies/Mushroom/Run (32x32).png")
+}
+
+type Sprite struct {
+	Image *ebiten.Image
+	X     float64
+	Y     float64
 }
 
 type Player struct {
-	X     float64
-	Y     float64
+	*Sprite
 	Vy    float64
 	Flip  bool
 	State string // "idle", "running", "jumping"
 }
 
+type Enemy struct {
+	*Sprite
+	State         string // "idle", "running", "jumping"
+	Flip          bool
+	FollowsPlayer bool
+}
+
 type Game struct {
-	Player  Player
+	Player  *Player
+	Enemys  []*Enemy
 	counter int
 	layers  [][]int
 }
@@ -56,7 +73,24 @@ func (g *Game) Update() error {
 	g.counter++
 	g.controlPlayer()
 	g.checkCollision()
+	g.controlEnemy()
 	return nil
+}
+
+func (g *Game) controlEnemy() {
+	for _, e := range g.Enemys {
+		if e.X < g.Player.X {
+			e.X += 1
+			e.Flip = true
+			e.State = "running"
+		} else if e.X > g.Player.X {
+			e.X -= 1
+			e.Flip = false
+			e.State = "running"
+		} else {
+			e.State = "idle"
+		}
+	}
 }
 
 func (g *Game) controlPlayer() {
@@ -140,6 +174,49 @@ func (g *Game) drawPlayer(screen *ebiten.Image, p *Player) {
 	screen.DrawImage(subImg, op)
 }
 
+func (g *Game) drawEnemy(screen *ebiten.Image, e *Enemy) {
+	var sx, sy int
+	switch e.State {
+	case "idle":
+		e.Sprite.Image = mushroomEnemyImageIdle
+		sx, sy = g.animateMushroomEnemyIdle()
+	case "running":
+		e.Sprite.Image = mushroomEnemyImageRun
+		sx, sy = g.animateMushroomEnemyRun()
+	default:
+		e.Sprite.Image = mushroomEnemyImageIdle
+		sx, sy = g.animateMushroomEnemyIdle()
+	}
+
+	subImg := e.Image.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image)
+
+	op := &ebiten.DrawImageOptions{}
+
+	cx := float64(frameWidth) / 2
+	cy := float64(frameHeight) / 2
+
+	op.GeoM.Translate(-cx, -cy)
+
+	if e.Flip {
+		op.GeoM.Scale(-1, 1)
+		op.GeoM.Translate(0, 0)
+	}
+
+	op.GeoM.Translate(e.X, e.Y)
+
+	screen.DrawImage(subImg, op)
+}
+
+func (g *Game) animateMushroomEnemyIdle() (int, int) {
+	i := (g.counter / 3) % 14
+	return frameOX + i*32, 0
+}
+
+func (g *Game) animateMushroomEnemyRun() (int, int) {
+	i := (g.counter / 5) % 16
+	return frameOX + i*32, 0
+}
+
 func (g *Game) renderGround(screen *ebiten.Image) {
 	w := tilesImage.Bounds().Dx()
 	tileXCount := w / tileSize
@@ -174,13 +251,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	skyColor := color.RGBA{R: 30, G: 30, B: 80, A: 255}
 	screen.Fill(skyColor)
 	g.renderGround(screen)
-	g.drawPlayer(screen, &g.Player)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()), 0, 0)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %0.2f", ebiten.CurrentFPS()), 0, 20)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("X: %0.2f", g.Player.X), 0, 40)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Y: %0.2f", g.Player.Y), 0, 60)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Vy: %0.2f", g.Player.Vy), 0, 80)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("State: %s", g.Player.State), 0, 100)
+	g.drawPlayer(screen, g.Player)
+	for _, e := range g.Enemys {
+		g.drawEnemy(screen, e)
+	}
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()), 10, 0)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("FPS: %0.2f", ebiten.CurrentFPS()), screenWidth-70, 0)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -190,11 +266,25 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 func main() {
 
 	game := &Game{
-		Player: Player{
-			X:     screenWidth / 2,
-			Y:     screenHeight - (frameHeight / 2) - tileSize,
+		Player: &Player{
+			Sprite: &Sprite{
+				Image: playerImage,
+				X:     screenWidth / 2,
+				Y:     screenHeight - (frameHeight / 2) - tileSize,
+			},
 			Flip:  false,
+			Vy:    0,
 			State: "idle",
+		},
+		Enemys: []*Enemy{
+			{
+				Sprite: &Sprite{
+					Image: mushroomEnemyImageIdle,
+					X:     screenWidth / 2,
+					Y:     screenHeight - (frameHeight / 2) - tileSize,
+				},
+				State: "idle",
+			},
 		},
 		layers: [][]int{
 			generateGroundLayer(),
@@ -208,12 +298,20 @@ func main() {
 	}
 }
 
-func loadImage(imageBytes []byte) *ebiten.Image {
+func loadImageBytes(imageBytes []byte) *ebiten.Image {
 	img, _, err := image.Decode(bytes.NewReader(imageBytes))
 	if err != nil {
 		log.Fatal(err)
 	}
 	return ebiten.NewImageFromImage(img)
+}
+
+func loadImage(path string) *ebiten.Image {
+	img, _, err := ebitenutil.NewImageFromFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return img
 }
 
 func generateGroundLayer() []int {
